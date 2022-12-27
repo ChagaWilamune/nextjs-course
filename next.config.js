24650567e3963147a6246b940bb554212dc651ad
chaga_w <chaga@konnect.team>
2022-12-27 23:59:55 +0530

    prepared for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: 'chaga1234',
+        mongodb_password: 'ohGjzCrwnyrW9mHx',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-app-dev',
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: 'chaga1234',
+      mongodb_password: 'ohGjzCrwnyrW9mHx',
+      mongodb_clustername: 'cluster0',
+      mongodb_database: 'my-app',
+    },
+  }
+};
